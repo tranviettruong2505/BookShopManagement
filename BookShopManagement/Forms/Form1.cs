7c1bc353ebe5607bd77bf04117e232fc9b4e7fcb
tranviettruong2505 <72683430+tranviettruong2505@users.noreply.github.com>
2021-05-30 16:49:46 +0700

    Initial commit

---
+++ BookShopManagement/Forms/Form1.cs
@@ -0,0 +1,49 @@
+﻿using BookShopManagement.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace BookShopManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string account = textBox1.Text;
+            string password = textBox2.Text;
+            if (Login_DAL.Instance.Staff_Infor(account,password).Rows.Count >0)
+            {
+                using (Form_Dashboard fd = new Form_Dashboard())
+                {
+                    fd.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BookShopManagement
+ 
